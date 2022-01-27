78ab1600911246fe6f575dc791c8e890d45b2933
Jatetanan Koh <68505570+jate-koh@users.noreply.github.com>
2022-01-27 16:06:48 +0000

    changed variable system code more compact

--- main.c
+++ main.c
@@ -3,119 +3,96 @@
 #include <stdlib.h>
 #include <string.h>
 
+//Converter Functions
+void Converter(int n,int ans[],int count);
+void SignMagnitude(int n,int ans[],int check,int count);
+void FirstComp(int n,int ans[],int check,int count);
+void SecondComp(int n,int ans[],int check,int count);
+//Misc. Functions
 int strCheck(char str[]);
-void Converter(int n);
-void SignMagnitude(int n);
-void FirstComp(int n);
-void SecondComp(int n);
+void init_array(int ans[],int count);
 void Print(int ans[],int count);
+
+int ans[32];
 
 int main() {
     char str[20];
-    int check=0;
+    int check=0,count=0;
+    printf("Input Decimal Number : ");
     gets(str);
     check = strCheck(str); 
     /* -2 = Negative Zero, -1 = Negative,
       0 = Positive Zero, 1 = Positive*/
     
-    int num = atoi(str);
-    
-    printf("0. Pre-conversion : ");
-    Converter(num);
+    printf("Number of bits [More than 4]\n");
+    scanf("%d",&count);
 
-    //printf("1. Sign-and-Magnitude");
+    while(count<4) {
+      printf("Please enter number more than 4\n");
+      count = 0;
+      scanf("%d",&count);
+    }
+
+    printf("%d-bits array generated",count);
+    Print(ans,count);
+    
+    int num = atoi(str);
+    //count = Converter(num,ans,check,count);
+    //init_array(ans,count);
+    printf("\nI. Sign-and-Magnitude");
+    SignMagnitude(num,ans,check,count);
+    init_array(ans,count);
+    printf("\nII. 1's Complement");
+    FirstComp(num,ans,check,count);
+    init_array(ans,count);
+    printf("\nIII. 2's Complement");
+    SecondComp(num,ans,check,count);
+    init_array(ans,count);
     
     return 0;
 }
 
-int strCheck(char str[]) { 
-    int i=0;
-    if(str[0]=='-') { //Negative
-      for(i=0;i<=strlen(str)-1;i++)
-        str[i] = str[i+1];
-      if(atoi(str)==0) return -2; //Negative Zero
-      else return -1;
-    }
-    else { //Positive
-      if(str[0]=='+')
-        for(i=0;i<=strlen(str)-1;i++)
-          str[i] = str[i+1];
-      if(atoi(str)==0) return 0; //Positivie Zero
-      else return 1;
+void Converter(int n,int ans[],int count) {
+    int index = count-1;
+    while(n>0) {
+        ans[index--] = n%2;
+        n = n/2;
     }
 }
 
-void Converter(int n) {
-    int ans[32], i=0, count=0, r=0, c=0;
-    ans[count++] = 0;
-    while(n>0) {
-        ans[count++] = n%2;
-        n = n/2;
+void SignMagnitude(int n,int ans[],int check,int count) {
+    Converter(n,ans,count);
+    if(check==-1 || check==-2) ans[0]=1;
+    else ans[0]=0;
+    Print(ans,count);
+}
+
+void FirstComp(int n,int ans[],int check,int count) {
+    Converter(n,ans,count);
+    if(check==-1 || check==-2) {
+      for(int i = 0; i<count; i++) {
+          if (ans[i] == 0) ans[i] = 1;
+          else ans[i] = 0;
+      }
     }
     Print(ans,count);
 }
 
-/*void SignMagnitude(int n) {
-    int ans[32],count=0,check=0;
+void SecondComp(int n,int ans[],int check,int count) {
     char str[20];
     ans[count++] = 0;
+    n -= 1;
     while(n>0) {
         ans[count++] = n%2;
         n = n/2;
     }
-    if(check == -1 || check == -2)
+    for(int i = 0; i<count; i++) {
+        if (ans[i] == 0) ans[i] = 1;
+        else ans[i] = 0;
+    }
+    /* for(int i = 0; i<count ; i++)
     {
-        ans[count]=1;
-    }
-    else{
-        ans[count]=0;
-    }
-    Print(ans,count);
-}*///ยังไม่ถูก
-
-void FirstComp(int n) {
-    int ans[32],count=0,check=0;
-    char str[20];
-    ans[count++] = 0;
-    while(n>0) {
-        ans[count++] = n%2;
-        n = n/2;
-    }
-    for(int i = 0; i<count; i++)
-    {
-        if (ans[i] == 0)
-        {
-            ans[i] = 1;
-        }
-        else
-        {
-            ans[i] = 0;
-        }
-    }
-    Print(ans,count);
-}
-
-void SecondComp(int n) {
-    int ans[32],count=0,check=0;
-    char str[20];
-    ans[count++] = 0;
-    while(n>0) {
-        ans[count++] = n%2;
-        n = n/2;
-    }
-    for(int i = 0; i<count; i++)
-    {
-        if (ans[i] == 0)
-        {
-            ans[i] = 1;
-        }
-        else
-        {
-            ans[i] = 0;
-        }
-    }
-    for(int i = 0; i<count ; i++)
-    {if(ans[count]==1)
+      if(ans[count]==1)
     {
         ans[count]=0;
                if(ans[count-1]==1)
@@ -146,8 +123,32 @@ void SecondComp(int n) {
     }
     else
         ans[count]=1;
-    }
+    } */
     Print(ans,count);
+}
+
+// Misc. Functions
+int strCheck(char str[]) { 
+    int i=0;
+    if(str[0]=='-') { //Negative
+      for(i=0;i<=strlen(str)-1;i++)
+        str[i] = str[i+1];
+      if(atoi(str)==0) return -2; //Negative Zero
+      else return -1;
+    }
+    else { //Positive
+      if(str[0]=='+')
+        for(i=0;i<=strlen(str)-1;i++)
+          str[i] = str[i+1];
+      if(atoi(str)==0) return 0; //Positivie Zero
+      else return 1;
+    }
+}
+
+void init_array(int ans[],int count) {
+  int i=0;
+  for(i=0;i<count;i++)
+    ans[i]=0;
 }
 
 void Print(int ans[],int count) {
