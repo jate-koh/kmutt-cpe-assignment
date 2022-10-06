a920e73d27d08e3154c6ab8906322bd26f74d909
jate-koh <jate_ben@hotmail.com>
2022-10-06 20:48:48 +0700

    feat: finish construct, accuracy, and error test

--- cpe327/Lab8/source/CalculatorTest.java
+++ cpe327/Lab8/source/CalculatorTest.java
@@ -16,28 +16,73 @@ public class CalculatorTest {
     public void constructorTest() {
         /* Test Blank Constructor */
         cal = new Calculator();
-        assertEquals("","0",String.valueOf(cal.getOperand1()));
-        assertEquals("","0",String.valueOf(cal.getOperand2()));
-        assertEquals("","0",String.valueOf(cal.calculate()));
+        assertEquals("0.0",String.valueOf(cal.getOperand1()));
+        assertEquals("0.0",String.valueOf(cal.getOperand2()));
+        assertEquals("0.0",String.valueOf(cal.calculate()));
+        assertEquals(true, cal.getError());
 
         /* Test String Constructor */
         cal = new Calculator("2","3","+");
-        assertEquals("","2",String.valueOf(cal.getOperand1()));
-        assertEquals("","3",String.valueOf(cal.getOperand1()));
-        assertEquals("","5",String.valueOf(cal.calculate()));
+        assertEquals("2.0",String.valueOf(cal.getOperand1()));
+        assertEquals("3.0",String.valueOf(cal.getOperand2()));
+        assertEquals("5.0",String.valueOf(cal.calculate()));
 
         cal = new Calculator(5,7,"-");
-        assertEquals("","5",String.valueOf(cal.getOperand1()));
-        assertEquals("","7",String.valueOf(cal.getOperand1()));
-        assertEquals("","-2",String.valueOf(cal.calculate()));
+        assertEquals("5.0",String.valueOf(cal.getOperand1()));
+        assertEquals("7.0",String.valueOf(cal.getOperand2()));
+        assertEquals("-2.0",String.valueOf(cal.calculate()));
     }
 
+    @Test
     public void accuracyTest() {
-        cal = new Calculator();
+
+        double max = 20.0; // Specify Maximum Test Case Here
+
+        /* Test Plus Sign */
+        cal = new Calculator(0,0,"+");
+        for(double i = 0.0; i < max ; i++) {
+            cal.setOperand1(i);
+            for(double j = 0.0; j < max; j++) {
+                cal.setOperand2(j);
+                assertEquals((String.valueOf(i+j)), String.valueOf((cal.calculate())));
+            }
+        }
+
+        /* Test Minus Sign */
+        cal = new Calculator(0,0,"-");
+        for(double i = 0.0; i < max ; i++) {
+            cal.setOperand1(i);
+            for(double j = 0.0; j < max; j++) {
+                cal.setOperand2(j);
+                assertEquals((String.valueOf(i-j)), String.valueOf((cal.calculate())));
+            }
+        }
+
+        /* Test Multiply */
+        cal = new Calculator(0,0,"*");
+        for(double i = 0.0; i < max ; i++) {
+            cal.setOperand1(i);
+            for(double j = 0.0; j < max; j++) {
+                cal.setOperand2(j);
+                assertEquals((String.valueOf(i*j)), String.valueOf((cal.calculate())));
+            }
+        }
+
+        cal = new Calculator(0,0,"/");
+        for(double i = 1.0; i < max ; i++) {
+            cal.setOperand1(i);
+            for(double j = 1.0; j < max; j++) {
+                cal.setOperand2(j);
+                assertEquals((String.valueOf(i/j)), String.valueOf((cal.calculate())));
+            }
+        }
     }
-
+    
+    @Test
     public void errorTest() {
-
+        cal = new Calculator(5,0,"/");
+        cal.calculate();
+        assertEquals(true, cal.getError());
     }
 
 }
